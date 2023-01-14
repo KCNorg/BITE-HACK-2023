b40fab82bd41973658dba135b49c0d4251dbc894
proman3419 <proman7268@gmail.com>
2023-01-14 10:42:33 +0100

    Init backend

---
+++ backend/bitehack2023/src/main/java/kcn/bitehack2023/Bitehack2023Application.java
@@ -0,0 +1,13 @@
+package kcn.bitehack2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bitehack2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bitehack2023Application.class, args);
+	}
+
+}
