ffd0a6946ac5140e4b1e734a83eb0ccea8a3dbd7
maati01 <mateuszpow7@gmail.com>
2023-01-14 21:28:51 +0100

    update

--- frontend/kcn_bitehack2023/src/App.js
+++ frontend/kcn_bitehack2023/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Auth from "./components/LoginForm/Auth"
+import HomePage from "./components/HomePage/HomePage"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Auth />} />
+        <Route path="/homePage" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
