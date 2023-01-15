40e670905ef894f984acadf1116c5f10c7686a91
Pawel Gorgolewski <pgorgolewski01@gmail.com>
2023-01-15 05:15:31 +0100

    create course site done

--- frontend/kcn_bitehack2023/src/components/CreateCourseTask/CreateCourseTask.js
+++ frontend/kcn_bitehack2023/src/components/CreateCourseTask/CreateCourseTask.js
@@ -3,19 +3,62 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import {Button} from "@mui/material";
 import Checkbox from '@mui/material/Checkbox';
-import {useLocation} from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import "./styles.css"
 
 export default function CreateCourseTask() {
 
     const location = useLocation();
-    //TODO title should be given from createCoursePage where it is seted
+    const navigate = useNavigate();
     let title = location.state.name;
 
     const displayCourseName = () => {
         return (
             <h4 className="Task-form-sub-title">{title}</h4>
         )
+    }
+
+    function handleFinishTask(e){
+        e.preventDefault();
+        navigate("/homePage");
+    }
+
+    function handleSubmitTask(e){
+        e.preventDefault()
+        fetch("http://localhost:8080/manageTask", {
+            method: "POST",
+            headers: {"Content-Type": "application/json", 'Accept': 'application/json'},
+            body: JSON.stringify({
+                _id: location.state._id,
+                item: {
+                    taskName: document.getElementById("task-name").value,
+                    note: document.getElementById("outlined-multiline-flexible my-text-fields").value,
+                    taskQuestion: document.getElementById("outlined-flexible my-task-quiz-question").value,
+                    answerList: [
+                        {
+                            question: document.getElementById("outlined-flexible my-question1").value,
+                            isCorrect: document.getElementById("my-checkbox1").checked
+                        },
+                        {
+                            question: document.getElementById("outlined-flexible my-question2").value,
+                            isCorrect: document.getElementById("my-checkbox2").checked
+                        },
+                        {
+                            question: document.getElementById("outlined-flexible my-question3").value,
+                            isCorrect: document.getElementById("my-checkbox3").checked
+                        }
+                    ]
+                }
+            })
+        })
+
+
+        // console.log(document.getElementById("task-name").value);
+        // console.log(document.getElementById("outlined-multiline-flexible my-text-fields").value);
+        // console.log(document.getElementById("outlined-flexible my-task-quiz-question").value);
+        // console.log(document.getElementById("outlined-flexible my-question1").value);
+        // console.log(document.getElementById("my-checkbox1").checked);
+        navigate(0, {state:location.state});
     }
 
 
@@ -25,7 +68,7 @@ export default function CreateCourseTask() {
                 <div className="Task-form-title">
                     {displayCourseName(title)}
                 </div>
-                <div className="Task-form-name-field">
+                <div className="Task-form-name-field" id="name-input-task">
                     <TextField
                         sx={{ m: 1, width: '60ch', margin: '17px' }}
                         id="task-name"
@@ -38,7 +81,7 @@ export default function CreateCourseTask() {
                         <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
                         <TextField
                             sx={{ m: 1, width: '60ch', margin: '17px' }}
-                            id="outlined-multiline-flexible"
+                            id="outlined-multiline-flexible my-text-fields"
                             label="Theory note"
                             multiline
                             rows={15}
@@ -58,7 +101,7 @@ export default function CreateCourseTask() {
                         >
                         <div>
                             <TextField
-                                id="outlined-flexible"
+                                id="outlined-flexible my-task-quiz-question"
                                 sx={{ m: 1, width: '45ch', margin: '17px' }}
                                 label="question..."
                                 placeholder="Question placeholder"
@@ -67,7 +110,7 @@ export default function CreateCourseTask() {
                                 fullWidth
                             />
                             <TextField
-                                id="outlined-flexible"
+                                id="outlined-flexible my-question1"
                                 sx={{ m: 1, width: '39ch', margin: '17px' }}
                                 label="answer 1.."
                                 placeholder="Question placeholder"
@@ -78,6 +121,7 @@ export default function CreateCourseTask() {
                             <Checkbox
                                 defaultChecked
                                 color="success"
+                                id="my-checkbox1"
                                 sx={{ '& .MuiSvgIcon-root': {
                                         fontSize: 28,
                                         marginTop: '20px'
@@ -85,7 +129,7 @@ export default function CreateCourseTask() {
                                 }}
                             />
                             <TextField
-                                id="outlined-flexible"
+                                id="outlined-flexible my-question2"
                                 sx={{ m: 1, width: '39ch', margin: '17px' }}
                                 label="answer 2.."
                                 placeholder="Question placeholder"
@@ -96,6 +140,7 @@ export default function CreateCourseTask() {
                             <Checkbox
                                 defaultChecked
                                 color="success"
+                                id="my-checkbox2"
                                 sx={{ '& .MuiSvgIcon-root': {
                                         fontSize: 28,
                                         marginTop: '20px'
@@ -103,7 +148,7 @@ export default function CreateCourseTask() {
                                 }}
                             />
                             <TextField
-                                id="outlined-flexible"
+                                id="outlined-flexible my-question3"
                                 sx={{ m: 1, width: '39ch', margin: '17px' }}
                                 label="answer 3.."
                                 placeholder="Question placeholder"
@@ -114,6 +159,7 @@ export default function CreateCourseTask() {
                             <Checkbox
                                 defaultChecked
                                 color="success"
+                                id="my-checkbox3"
                                 sx={{ '& .MuiSvgIcon-root': {
                                         fontSize: 28,
                                         marginTop: '20px'
@@ -130,12 +176,14 @@ export default function CreateCourseTask() {
                         <Button
                             variant="contained"
                             fullWidth='on'
+                            onClick={handleSubmitTask}
                             sx={{ marginTop: '17px' }}>
                             Submit Task
                         </Button>
                         <Button
                             variant="contained"
                             fullWidth='in'
+                            onClick={handleFinishTask}
                             sx={{ marginTop: '17px' }}
                             color="success">
                             Finish Course
