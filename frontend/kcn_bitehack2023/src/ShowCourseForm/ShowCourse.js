05ca84cc261ec61a55bd99154b5ed22a30e622e9
proman3419 <proman7268@gmail.com>
2023-01-15 10:08:45 +0100

    Added a question

--- frontend/kcn_bitehack2023/src/ShowCourseForm/ShowCourse.js
+++ frontend/kcn_bitehack2023/src/ShowCourseForm/ShowCourse.js
@@ -30,8 +30,8 @@ function ShowCourse() {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const activities = [
-    new Activity(0, "T1", `There are four types of Java access modifiers:
-    Private: The access level of a private modifier is only within the class. It cannot be accessed from outside the class.
+    new Activity(0, "T1", `There are four types of Java access modifiers:<br>
+    Private: The access level of a private modifier is only within the class. It cannot be accessed from outside the class.<br>
     Default: The access level of a default modifier is only within the package. It cannot be accessed from outside the package. If you do not specify any access level, it will be the default.
     Protected: The access level of a protected modifier is within the package and outside the package through child class. If you do not make the child class, it cannot be accessed from outside the package.
     Public: The access level of a public modifier is everywhere. It can be accessed from within the class, outside the class, within the package and outside the package.`, ["Good one1", "Interesting1sssssssss", "Inspiring1"]),
